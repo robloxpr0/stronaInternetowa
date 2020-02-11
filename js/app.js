79db2005feb8bc1fe20218a959a5c1c34044b143
robloxpr0 <robertp2208@gmail.com>
2020-02-11 20:25:36 +0100

    Content, footer

--- js/app.js
+++ js/app.js
@@ -3,9 +3,14 @@ moveToPage.onclick = mainPage;
 function mainPage(){
     coverImage.classList.toggle('move');
     setTimeout(displayNone, 1000);
+    setTimeout(showContent, 700);
 }
 
 function displayNone(){
     coverImage.style.display = 'none';
 }
 
+function showContent(){
+    container.style.display = 'block';
+    container.classList.toggle('showContainer');
+}
