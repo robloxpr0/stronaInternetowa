c07009201dbf16543d00ecad2eea7d4471a8a269
robloxpr0 <robertp2208@gmail.com>
2020-02-11 18:17:48 +0100

    Slider, navbar i logo

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+moveToPage.onclick = mainPage; 
+
 function mainPage(){
-    
+    coverImage.classList.toggle('move');
+    setTimeout(displayNone, 1000);
 }
+
+function displayNone(){
+    coverImage.style.display = 'none';
+}
+
