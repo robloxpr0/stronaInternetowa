0bb58f895c6f77ed900e8772c853c954b0c8aa05
robloxpr0 <robertp2208@gmail.com>
2020-02-13 22:53:09 +0100

    Changes

--- js/app.js
+++ js/app.js
@@ -1,40 +1,39 @@
-hamburger.onclick = mobileShow;
-function mobileShow(){
-    menuMobile.classList.toggle('is-active');
+hamburger.onclick = hamburgerClick;
+function hamburgerClick() {
+    menuList.classList.toggle('is-active');
     line1.classList.toggle('change1');
     line2.classList.toggle('change2');
     line3.classList.toggle('change3');
-
 }
-window.onscroll = mojaFunkcja;
-let navbar = document.getElementById('menu');
-let scroll = navbar.offsetTop;
-
-function mojaFunkcja(){
-    if(window.pageYOffset >= scroll){
-        navbar.classList.add('scrolled');
-        slider.classList.add('margin');
-    } else{
-        navbar.classList.remove('scrolled');
-        slider.classList.remove('margin');
+let menuHeight;
+let navOff = menu.offsetTop;
+window.onscroll = fixedNav;
+function fixedNav(){
+    menuHeight = menu.offsetHeight;
+    if(pageYOffset >= navOff){
+        menu.classList.add('menuFixed');
+        air.style.height = menuHeight + 'px';
+    } else {
+        menu.classList.remove('menuFixed');
+        air.style.height = 0;
     }
 }
 
-fb.addEventListener('touchstart', function(){
+fb.addEventListener('touchstart', function () {
     fb.classList.add('fbHover');
 });
-fb.addEventListener('touchend', function(){
+fb.addEventListener('touchend', function () {
     fb.classList.remove('fbHover');
 });
-insta.addEventListener('touchstart', function(){
+insta.addEventListener('touchstart', function () {
     insta.classList.add('inHover');
 });
-insta.addEventListener('touchend', function(){
+insta.addEventListener('touchend', function () {
     insta.classList.remove('inHover');
 });
-yt.addEventListener('touchstart', function(){
+yt.addEventListener('touchstart', function () {
     yt.classList.add('ytHover');
 });
-yt.addEventListener('touchend', function(){
+yt.addEventListener('touchend', function () {
     yt.classList.remove('ytHover');
 });
