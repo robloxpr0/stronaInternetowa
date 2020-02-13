4a800589c15519222f1a99639b965c818f313106
robloxpr0 <robertp2208@gmail.com>
2020-02-13 14:12:09 +0100

    Navbar fixed, images, different page animations.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,22 @@
-hamburger.onclick = MobileShow;
-function MobileShow(){
+hamburger.onclick = mobileShow;
+function mobileShow(){
     menuMobile.classList.toggle('is-active');
     line1.classList.toggle('change1');
     line2.classList.toggle('change2');
     line3.classList.toggle('change3');
+
 }
+window.onscroll = mojaFunkcja;
+let navbar = document.getElementById('menu');
+let scroll = navbar.offsetTop;
+
+function mojaFunkcja(){
+    if(window.pageYOffset >= scroll){
+        navbar.classList.add('scrolled');
+        slider.classList.add('margin');
+    } else{
+        navbar.classList.remove('scrolled');
+        slider.classList.remove('margin');
+    }
+}
+
